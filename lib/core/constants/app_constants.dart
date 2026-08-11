321357c1d9e1c59f9c474f629cbaec2cda74749c
Saikiran Personal <saikiranlingampally26@gmail.com>
2026-08-11 10:14:40 +0530

    Updated resume 1.5

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -10,7 +10,9 @@ class AppConstants {
   static const connectText='Connect';
   static const downloadCVText='Download CV';
   static const professionalExperienceText='Professional Experience';
-  static const fileId = '1hLhQsAW4f7ZG_VbuFwtpBqCOgpg9Bz_h';
-  // 1sGGAFo_IE5joYtGieUGMdTZb4bvmYK9K
+  static const fileId = '1Fm4RxQeqvHwUH4HnMeOXcDSYhQoCvGzT';
+//https://drive.google.com/file/d/1Fm4RxQeqvHwUH4HnMeOXcDSYhQoCvGzT/view?usp=drive_link
+//'1hLhQsAW4f7ZG_VbuFwtpBqCOgpg9Bz_h';
+  // 1sGGAFo_IE5joYtGieUGMdTZb4bvmYK9K  https://drive.google.com/file/d/1fwttP5FyHkPCwyvHHn6YzA7iLkKxW60l/view?usp=drive_link
 
 }
